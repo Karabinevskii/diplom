cdf3da192591faf62d0e175c8ebcd1817d139847
Karabinevskii Alexandr <karabinevski@mail.ru>
2024-06-07 00:09:49 +0300

    diplom update

--- test_ui/pages/catalog_page.py
+++ test_ui/pages/catalog_page.py
@@ -1,13 +1,20 @@
+import allure
+import pytest
+
 from test_ui.locators.catalog_page_locators import BN_CATALOG, BN_COMPUTERS, BN_LAPTOP
 from test_ui.pages.base_page import BasePage
 
 
+@allure.feature("Catalog Page Tests")
 class CatalogPage(BasePage):
+    @allure.title("Test clicking on catalog")
     def click_catalog(self):
         self.click(BN_CATALOG)
 
+    @allure.title("Test selecting computers")
     def computers(self):
         self.click(BN_COMPUTERS)
 
+    @allure.title("Test choosing a laptop")
     def choosing_laptop(self):
         self.click(BN_LAPTOP)
